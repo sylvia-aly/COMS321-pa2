cb47a6e946db887c6162cd297db09ca0569f727c
Neha Maddali <89538622+nmaddali03@users.noreply.github.com>
2022-04-13 20:52:47 -0500

    Add files via upload

---
+++ disassembler/src/disassembler/disassembler.java
@@ -0,0 +1,68 @@
+package disassembler;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+/*
+ * Given in the piazza post of programming assignment 2
+ * 
+ * //This is in main():
+    if (binary) {
+      fd = open(argv[1], O_RDONLY);
+      fstat(fd, &buf);
+      program = mmap(NULL, buf.st_size, PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE, fd, 0);
+      bprogram = calloc(buf.st_size / 4, sizeof (*bprogram));
+      for (i = 0; i < (buf.st_size / 4); i++) {
+        program[i] = be32toh(program[i]);
+        decode(program[i], bprogram + i);
+      }
+      emulate(bprogram, buf.st_size / 4, &m);
+
+      return 0;
+    }
+ */
+
+public class disassembler {
+	private static ArrayList<String> assemblyInstructions;
+    private static ArrayList<String> instructionsThatNeedLabels = new ArrayList<>();
+    private static ArrayList<Integer> posOfLabels = new ArrayList<>();
+    //private static HashMap<String, String> listOfLabels = new HashMap<>();
+    
+    public static void main(String[] args) throws IOException {
+        instructionsThatNeedLabels = new ArrayList<>();
+        posOfLabels = new ArrayList<>();
+        //listOfLabels = new HashMap<>();
+        String instruction = "";
+        ArrayList<String> binary = new ArrayList<>();
+        File file = new File(args[0]);
+        FileInputStream sc = new FileInputStream(file);
+        byte[] inst = new byte[4];
+        while (sc.read(inst) != -1) {
+            for (int i = 0; i < 4; i++) {
+                // convert byte to binary string representation
+                instruction += String.format("%8s", Integer.toBinaryString(inst[i] & 0xFF)).replace(' ', '0');
+            }
+            binary.add(instruction);
+            instruction = "";
+        }
+        convertBinaryToAssembly(binary);
+        addLabels();
+        printAssembly(assemblyInstructions);
+    }
+    
+    public static void convertBinaryToAssembly(ArrayList<String> instructions) {
+    	
+    }
+    
+    public static void addLabels() {
+    	
+    }
+    
+    public static void printAssembly(ArrayList<String> instructions) {
+        for (String instruction : instructions) {
+            System.out.println(instruction);
+        }
+    }
+}
